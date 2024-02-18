cafe30d66ff00ce4062c0c9fcf5ceefe93dcfe8c
lowwie ee <ee.lowwie@gmail.com>
2024-02-18 17:23:46 -0500

    02-18-2024 Added Lesson 3

--- start_lessons.sh
+++ start_lessons.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-source ./kismet-env/bin/activate
-jupyter-notebook 
+
+echo "Do you want to start Jupyter Notebook on localhost or eth0?"
+read -p "Enter your choice (localhost/eth0): " choice
+
+if [ "$choice" == "localhost" ]; then
+  echo "Starting Jupyter Notebook on localhost..."
+  jupyter notebook --no-browser --port=8888 --ip=127.0.0.1
+elif [ "$choice" == "eth0" ]; then
+  echo "Starting Jupyter Notebook on eth0..."
+  jupyter notebook --no-browser --port=8888 --ip=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
+else
+  echo "Invalid choice. Please enter either 'localhost' or 'eth0'."
+fi
